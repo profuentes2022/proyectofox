c377e990c6f6bc5c35d1cb50b8cbca354a4d0459
profuentes2022 <100309580+profuentes2022@users.noreply.github.com>
2023-08-08 15:42:06 -0600

    extraccion
    estraccion de datos

--- back/index.js
+++ back/index.js
@@ -9,7 +9,7 @@ app.use(bodyParse.json());
 app.use(cors({origin:'*'}));
 
 const config={
-server:'bwesvytlgr2xmrmbfmm5-mysql.services.clever-cloud.com',
+host:'bwesvytlgr2xmrmbfmm5-mysql.services.clever-cloud.com',
 user:'ut0fvej3gvvbvt5j',
 password:'8UOyZkaMNUFHo87VQRhf',
 database:'bwesvytlgr2xmrmbfmm5'
@@ -25,12 +25,12 @@ app.post("/alumnos",(req,res)=>{
    let nombre=req.body.nombre;
    let grado=req.body.grado;
    let seccion=req.body.seccion
-   let aciento =req.body.asiento;
+   let aciento =req.body.aciento;
    let fecha =req.body.fecha;
    let  entrada =req.body.entrada;
    let  salida =req.body.salida;
-   console.log(`INSERT INTO alumnos (codigo, nombre, grado, seccion, aciento, fecha, entrada, salida) VALUES (${codigo},${nombre},${grado},${seccion},${aciento},${fecha},${entrada},${salida})`);
-pool.query(`INSERT INTO alumnos (codigo, nombre, grado, seccion, aciento, fecha, entrada, salida) VALUES (${codigo},${nombre},${grado},${seccion},${aciento},${fecha},${entrada},${salida})`,(err,result)=>{
+
+pool.query(`INSERT INTO alumnos (codigo, nombre, grado, seccion, aciento, fecha, entrada, salida) VALUES ('${codigo}','${nombre}','${grado}','${seccion}','${aciento}','${fecha}','${entrada}','${salida}')`,(err,result)=>{
 if (err){
 }
 res.send("ok");
@@ -39,11 +39,29 @@ res.send("ok");
 
 })
 
+app.get("/alumnos",(req,res)=>{
+let alumno = req.query.codigo;
+
+
+pool.query(`select * from alumnos where codigo =${alumno}` ,(err,result)=>{
+    if (err){
+    }
+    res.send(result);
+    })
+
+
+});
+
+
+
 
 
 app.get("/",(req,res)=>{
 
 })
+
+
+
 
 app.listen(5555,()=>{
     console.log("servidor activo ");
